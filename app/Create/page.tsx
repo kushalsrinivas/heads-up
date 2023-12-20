3f99372067a75de0fbdbd73b2d9ff627c0206a52
kushalsrinivas <kushal.s.2005@gmail.com>
2023-12-20 23:05:52 +0530

    updated src file

--- app/Create/page.tsx
+++ app/Create/page.tsx
@@ -8,7 +8,13 @@ import { useStoreContext } from "../Context/Store";
 import { SiPubg } from "react-icons/si";
 import { Calendar } from "@/components/ui/calendar";
 import Image from "next/image";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import {
   Popover,
@@ -16,6 +22,7 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Textarea } from "@/components/ui/textarea";
+import { useMultistepForm } from "@/lib/useMutlistepform";
 
 function Home() {
   const ctx = useStoreContext();
@@ -39,20 +46,17 @@ function Home() {
       img: "https://i.pinimg.com/originals/ab/40/f4/ab40f48fae759e94ee9c636bbbcba00d.png",
     },
   ];
-
-  return (
-    <div className="flex justify-center flex-col w-1/2 m-auto gap-3 clear">
-      <div className="h-[65vh] flex flex-col gap-3">
-        <div>
-          <Label className="text-3xl font-bold ">Name </Label>
-          <Input
-            placeholder="Valorant epic battle"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div>
-          <Label className="text-3xl font-bold ">Description </Label>
+  const { steps, currentStepIndex, step, next, back, isLastStep, isFirstStep } =
+    useMultistepForm([
+      <div key={123123}>
+        <Label className="text-xl font-semibold  ">Name </Label>
+        <Input
+          placeholder="Valorant epic battle"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <div className="mt-3">
+          <Label className="text-xl font-semibold  ">Description </Label>
           <Textarea
             maxLength={2000}
             cols={15}
@@ -62,8 +66,9 @@ function Home() {
             onChange={(e) => setDesc(e.target.value)}
           />
         </div>
-      </div>
-      <div className="h-full w-full ">
+      </div>,
+
+      <div key={32424} className="h-full w-full ">
         <Label className="text-3xl font-bold ">Game</Label>
         <div className="flex lg:flex-row gap-3 flex-col h-full w-full p-5">
           {games.map((item, id) => {
@@ -92,49 +97,79 @@ function Home() {
             );
           })}
         </div>
-      </div>{" "}
-      <div>
-        <Label className="text-3xl font-bold ">Number of players </Label>
-        <Input
-          placeholder="1"
-          type="number"
-          value={NoPlayer}
-          onChange={(e) =>
-            setNP(parseInt(e.target.value) > 0 ? e.target.value : 0)
-          }
-        />
-      </div>
-      <div className="flex flex-col gap-5">
-        <Label className="text-3xl font-bold ">Tournament date </Label>
+      </div>,
+
+      <div key={12938129839108}>
+        <div>
+          <Label className="text-3xl font-bold ">Number of players </Label>
+          <Input
+            placeholder="1"
+            type="number"
+            value={NoPlayer}
+            onChange={(e) =>
+              setNP(parseInt(e.target.value) > 0 ? e.target.value : 0)
+            }
+          />
+        </div>
         <div className="flex flex-col gap-5">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn("w-full pl-3 text-left font-normal")}
-              >
-                {d.toDateString(Schedule)}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                mode="single"
-                onSelect={(date: any) => {
-                  setSchedule(new Date().toDateString(date));
-                  console.log("====================================");
-                  console.log(date);
-                  console.log("====================================");
-                  console.log("sdfsdfsd", new Date().toDateString(date));
-                }}
-                disabled={
-                  (date) => date <= new Date()
-                  // || date < new Date("1900-01-01")
-                }
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-          <Button>Publish tournament</Button>
+          <Label className="text-3xl font-bold ">Tournament date </Label>
+          <div className="flex flex-col gap-5">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  className={cn("w-full pl-3 text-left font-normal")}
+                >
+                  {d.toDateString(Schedule)}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="single"
+                  onSelect={(date: any) => {
+                    setSchedule(new Date().toDateString(date));
+                    console.log("====================================");
+                    console.log(date);
+                    console.log("====================================");
+                    console.log("sdfsdfsd", new Date().toDateString(date));
+                  }}
+                  disabled={
+                    (date) => date <= new Date()
+                    // || date < new Date("1900-01-01")
+                  }
+                  initialFocus
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+        </div>
+      </div>,
+    ]);
+  return (
+    <div className="flex justify-center flex-col w-1/2 m-auto gap-3 clear">
+      <div className="h-[65vh] flex flex-col gap-3">
+        <div>
+          <Card>
+            <CardHeader className="flex justify-between flex-row items-center w-full ">
+              <CardTitle>Create your tournament</CardTitle>
+              <CardTitle>
+                {currentStepIndex + 1} / {steps.length}
+              </CardTitle>
+            </CardHeader>
+            <CardContent>{step}</CardContent>
+            <CardFooter className="flex flex-row justify-between">
+              {isFirstStep ? null : (
+                <Button variant={"outline"} onClick={back}>
+                  back
+                </Button>
+              )}
+              {isLastStep ? (
+                <Button variant={"destructive"}>Publish</Button>
+              ) : (
+                <Button onClick={next}>next</Button>
+              )}
+            </CardFooter>
+          </Card>
         </div>
       </div>
     </div>
